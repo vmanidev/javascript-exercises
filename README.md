0fabcb8d43add1223c0285f2578f6fc2e9f6a5b2
vmanidev <vmanidev@outlook.com>
2025-08-02 23:39:37 +0530

    feat: implement prime number checker

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ A collection of JavaScript exercises for practicing core JavaScript concepts.
 1. Reverse a string 
 2. FizzBuzz 
 3. Check for palindrome 
-4. Sum of Numbers in an Array 
+4. Sum of numbers in an array 
+5. Check if a number is prime 
---
+++ basic/primeNumber.js
@@ -0,0 +1,17 @@
+const userInput = window.prompt('Enter a number to check if prime or not');
+
+if (userInput !== null) {
+    const number = parseInt(userInput, 10); //convert input string to an integer using base 10
+    if (!isNaN(number)) {
+        if (isPrime(number)) console.log(`${number} is a prime number`);
+        else console.log(`${number} is not a prime number`);
+    }
+}
+
+function isPrime(number) {
+    if (number <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) return false;
+    }
+    return true;
+}
